22a187d857e078eb00ced66d39eb55e923536239
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-03-29 10:46:30 +0530

    First Commit

---
+++ AzureDatabaseProject/Models/ShowContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDatabaseProject.Models
+{
+    public class ShowContext : DbContext
+    {
+        public ShowContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 101,
+                Name = "X-Men 1",
+                Description = "Sci-Fi",
+                Duration = 122.2f
+            });
+        }
+    }
+}
